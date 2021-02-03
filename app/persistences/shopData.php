940745d17a88b903d4b73a2ee5a7daef763c5bd7
SteveBotella <steve.botella@le-campus-numerique.fr>
2021-02-03 14:14:39 +0100

    feat: function getAllProducts

--- app/persistences/shopData.php
+++ app/persistences/shopData.php
@@ -1,9 +1,18 @@
 <?php
-
+//Fonction qui récupère un produit suivant son ID
 function productById (PDO $mydb, int $idProduct){
   $statement = $mydb->query ("SELECT title, description, id, price_ht, vat
 FROM products
 WHERE id = $idProduct");
 $result= $statement ->fetch(PDO::FETCH_ASSOC);
 return $result;
+}
+
+//Fonction qui recupère tous les produits
+function getAllProducts(PDO $mydb){
+    $statement = $mydb->query ("SELECT title, description, price_ht, vat
+    FROM products
+    ORDER BY title");
+    $result = $statement -> fetchAll(PDO::FETCH_ASSOC);
+    return $result;
 }
